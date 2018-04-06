b89345c3677349fa8cacc1e08e26c81014620265
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-04-06 20:17:07 +0530

    Create balanced.py

---
+++ balanced.py
@@ -0,0 +1,22 @@
+def parChecker(symbolString):
+  s=Stack()
+  balanced=True
+  index=0
+  while index<len(symbolString)
+  symbol=symbolString[index]
+  if symbol=="(":
+    s.push(symbol)
+  else:
+    if s.isEmpty():
+      balanced=False
+      else:
+        s.pop()
+  index=index+1
+  if balanced and s.isEmpty():
+    return True
+  else:
+    return False
+print(parChecker('((()))'))    
+        
+   
+  
