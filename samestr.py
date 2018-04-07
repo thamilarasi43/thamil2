f2552ca9cc00d8ad507dbc0f10dd918e12cc0391
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-04-07 10:09:46 +0530

    Create samestr.py

---
+++ samestr.py
@@ -0,0 +1,6 @@
+string1 = 'Hello'
+string2 = 'hel'
+if string1.lower() == string2.lower():
+    print("The strings are the same case insensitive")
+else:
+    print("The strings are not the same case insensitive")
