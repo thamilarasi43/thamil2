38b608138f10b5591ce3e9bd1cfc31f08492c71a
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-04-07 10:11:56 +0530

    Create strsame.py

---
+++ strsame.py
@@ -0,0 +1,6 @@
+string = 'Hello'
+string2 = 'hel'
+if string.lower() == string2.lower():
+    print("The strings are the same case insensitive")
+else:
+    print("The strings are not the same case insensitive")
